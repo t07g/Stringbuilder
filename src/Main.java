ed6e8c6ed598d6685b828e301c5532d69302aa11
t07g <tanmay0638@gmail.com>
2025-06-23 08:42:35 +0530

    init commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        StringBuilder sb = new StringBuilder("tony");
+        System.out.println(sb);
+
+        // Char at index 1
+        System.out.println(sb.charAt(1));
+
+        // set char at 0
+        sb.setCharAt(0,'p');
+        System.out.println(sb);
+
+    }
+}
